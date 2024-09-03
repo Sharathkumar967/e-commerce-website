149de10b2af5cf3b058c4d3e65db1c9b69eabb67
sharath kumar <sharath.kumar@motion.ac.in>
2024-09-03 13:03:01 +0530

    admin panel completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Admin from "./Pages/Admin/Admin";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Admin />
+    </div>
+  );
 };
 
 export default App;
