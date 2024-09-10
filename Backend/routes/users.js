5634d51b318dacca56f3532faab6706dde8f6fb9
sharath kumar <sharath.kumar@motion.ac.in>
2024-09-10 15:51:02 +0530

    installed dotenv

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const Users = require("../models/User");
--- Backend/utils/token.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (user) => {
-  const data = { user: { id: user.id } };
-  return jwt.sign(data, "secret_ecom");
-};
-
-module.exports = { generateToken };
